1ebb862df0ebc3042dc0ad95e94ba372a7365f50
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-30 10:21:31 +0100

    add full CRUD on user model with new route, tested with postman

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use("/api", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/api", require("./routes/question.routes"));
 app.use("/api", require("./routes/answer.routes"));
+app.use("/api", require("./routes/user.routes"));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
