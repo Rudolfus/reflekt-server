b45222a69c4e220091e934d5987c0d3b3017bbad
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-03-04 18:17:26 +0100

    add keep me alive function

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const Question = require("../models/Question.model");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+// keep me alive: Sends an http request to an endpoint that makes an operation with the database.
+
+const QuestionModel = require("../models/Question.model");
+
+router.get("/keep-alive", (req, res, next) => {
+  Question.find()
+    .then(() => {
+      res.status(200).json({ message: "It worked" });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "It didn't work" });
+    });
 });
 
 module.exports = router;
